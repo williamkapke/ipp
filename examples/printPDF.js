e5edf6bcaf670233a9a7ed35ebca44bd3ae0758c
William Wicks <wjwicks@gmail.com>
2013-03-01 14:02:25 +0000

    removed duplicated code

--- examples/printPDF.js
+++ examples/printPDF.js
@@ -19,27 +19,3 @@ doc.output(function(pdf){
 		console.log(res);
 	});
 });
-
-
-
-var ipp = require('ipp');
-var PDFDocument = require('pdfkit');
-
-//make a PDF document
-var doc = new PDFDocument({margin:0});
-doc.text(".", 0, 780);
-
-doc.output(function(pdf){
-	var printer = ipp.Printer("http://NPI977E4E.local.:631/ipp/printer");
-	var msg = {
-		"operation-attributes-tag": {
-			"requesting-user-name": "William",
-			"job-name": "My Test Job",
-			"document-format": "application/pdf"
-		},
-		data: pdf
-	};
-	printer.execute("Print-Job", msg, function(err, res){
-		console.log(res);
-	});
-});
